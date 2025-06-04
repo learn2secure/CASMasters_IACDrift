3111f7a9878497e1c949d855293e84417757a51a
PL <peleung@paloaltonetworks.com>
2025-06-04 15:22:31 -0400

    add AWS s3 bucket resources via Terraform for drift demo

--- s3.tf
+++ s3.tf
@@ -1,10 +1,11 @@
 provider "aws" {
-  region     = "eu-XXXXX-1"
-  access_key = "XXXXXXXXXXX
-  secret_key = "XXXXXXXXXXXX"
+  region = "us-east-2"
 }
 
-resource "aws_s3_bucket" "jmdrift-bucket" {
-  bucket = "jm-this-bucket-should-drift"
-  
+resource "aws_s3_bucket" "pldrift-bucket" {
+  bucket = "pl2025-this-bucket-should-drift"
+  tags = {
+    yor_name  = "pldrift-bucket"
+    yor_trace = "87f4433d-60fb-46e9-a418-96276c4dc4af"
+  }
 }
